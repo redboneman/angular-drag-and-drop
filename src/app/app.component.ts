1f463c38e807b63f3633a94428c56bb1ef3be407
a.lokhvitskiy <a.lokhvitskiy@gagaringroup.com>
2024-03-01 14:46:47 +0000

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,115 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {
+	CdkDrag,
+	CdkDragDrop,
+	CdkDropList,
+	CdkDropListGroup,
+	moveItemInArray,
+	transferArrayItem
+} from '@angular/cdk/drag-drop';
+import {CdkContextMenuTrigger, CdkMenu, CdkMenuItem} from '@angular/cdk/menu';
+import {NgStyle} from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, CdkDrag, CdkDropList, CdkDropListGroup, CdkContextMenuTrigger, CdkMenu, CdkMenuItem, NgStyle],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-drag-and-drop';
+	public availableRowsOptions = [
+		{
+			columns: 1,
+			title: 'One column'
+		},
+		{
+			columns: 2,
+			title: 'Two columns'
+		},
+		{
+			columns: 3,
+			title: 'Three columns'
+		},
+		{
+			columns: 4,
+			title: 'Four columns'
+		}
+	]
+
+	public availableColumnsOptions = [
+		{
+			color: '#ff0000',
+			title: 'Red'
+		},
+		{
+			color: '#00ff00',
+			title: 'Green'
+		},
+		{
+			color: '#0000ff',
+			title: 'Blue'
+		},
+	]
+
+	public selectedRows = [
+		{
+			columns: [
+				null,
+				null,
+				null
+			]
+		},
+		{
+			columns: [
+				null
+			]
+		},
+		{
+			columns: [
+				null,
+				null
+			]
+		}
+	]
+
+	public dropRow(event: CdkDragDrop<any>) {
+		console.log(event)
+		if (event.previousContainer !== event.container) {
+			const draggedRow = this.availableRowsOptions[event.previousIndex]
+			const preparedRow = {
+				columns: Array.from({length: draggedRow.columns}, () => null)
+			}
+			console.log(preparedRow)
+			this.insertToArray(this.selectedRows, event.currentIndex, preparedRow)
+		} else {
+			moveItemInArray(this.selectedRows, event.previousIndex, event.currentIndex)
+		}
+	}
+
+	public dropCol(event: CdkDragDrop<any>, rowIndex: number) {
+		console.log(event)
+		if (event.previousContainer !== event.container) {
+			if (event.container.element.nativeElement.classList.contains('row')) {
+				transferArrayItem(
+					event.previousContainer.data,
+					event.container.data,
+					event.previousIndex,
+					event.currentIndex
+				)
+			}
+		} else {
+			moveItemInArray(this.selectedRows[rowIndex].columns, event.previousIndex, event.currentIndex)
+		}
+	}
+
+	private insertToArray(arr: any[], index: number, newItem: any) {
+		arr.splice(index, 0, newItem)
+	}
+
+	public changeCol(row: number, col: number, color: any) {
+		this.selectedRows[row].columns[col] = color.color
+		console.log(this.selectedRows)
+	}
 }
